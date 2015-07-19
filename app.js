a98f033689cce123a2313a9862014126bfed477b
Simon Sun <blackghost0118@gmail.com>
2015-07-19 16:04:18 +0800

    adjust some init code

--- app.js
+++ app.js
@@ -9,18 +9,30 @@ var express = require('express');
 
 var app = express();
 
-// 載入全域變數與設定
+
+/*
+ * 載入全域變數與設定
+ */
 require('./Global');
 
-// 載入所有的 middleware 與設定
+
+/*
+ * 載入所有的 middleware 與設定
+ */
 var middlewares = require('./middlewares')(app);
 app.use(middlewares);
 
-// 載入 controller
+
+/*
+ * 載入 controller
+ */
 var controllers = require('./controllers')(app);
 app.use(controllers);
 
-// 載入 errorHandler
+
+/*
+ * 載入 errorHandler
+ */
 var errorHandler = require('./errorHandler')(app);
 app.use(errorHandler);
 
