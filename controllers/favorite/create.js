efe88145e610452b2ea344791916095e27aa75f1
webber_wu <webber_wu@hiiir.com>
2015-07-25 16:29:24 +0800

    出包了卡關 [favorite]

--- controllers/favorite/create.js
+++ controllers/favorite/create.js
@@ -1,4 +1,21 @@
 
 module.exports = function(req, res, next) {
-    return res.api(req.body);
+    var data = req.body;
+    return models.favorite.createAsync({
+        location: {
+            id: data.id,
+            name: data.name,
+            category: data.category,
+            latitude: data.latitude,
+            longitude: data.longitude,
+            checkins: data.checkins,
+            checkins_upcount: data.checkins_upcount,
+            startdate: data.startdate
+        },
+        createdBy: data.userId
+    })
+    .then(function(favorite) {
+        return res.api(favorite);
+    })
+    .catch(next);
 };
