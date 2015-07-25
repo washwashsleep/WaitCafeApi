2e0e8878b0c6fd910b5f607334eeec47e4f176a0
Simon Sun <blackghost0118@gmail.com>
2015-07-25 14:12:41 +0800

    create empty create for favorite

--- controllers/favorite/index.js
+++ controllers/favorite/index.js
@@ -3,12 +3,15 @@ var express = require('express');
 var router = express.Router();
 
 var Recommend = require('./recommend');
-// var Create = require('./create');
+var Create = require('./create');
 // var Login = require('./login');
 
 var noop = function(req, res, next) {
     res.status(200).send();
 };
+
+router.route('/favorite')
+    .post(Create);
 
 router.route('/favorite/recommend')
     .get(Recommend);
