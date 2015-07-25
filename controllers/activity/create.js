493d1a621f6532c9df79d88f4bb5625c4377eec6
Simon Sun <blackghost0118@gmail.com>
2015-07-25 16:26:24 +0800

    創造活動前先檢查是否已經是在活動當中

--- controllers/activity/create.js
+++ controllers/activity/create.js
@@ -3,15 +3,28 @@ module.exports = function(req, res, next) {
 
     var data = req.body;
 
-    return models.activity.createAsync({
-        location: data.location,
-        createdBy: data.userId,
-        start: data.start,
-        end: data.end
-    })
-    .then(function(activity) {
-        return res.api(activity);
-    })
-    .catch(next);
+    return models.activity.findOne()
+        .where('createdBy').equals(data.userId)
+        .where('start').lt(Date.now())
+        .where('end').gt(Date.now())
+        .where('trashed').equals(false)
+        .execAsync()
+        .then(function(activity) {
+
+            if(activity){
+                return Promise.reject(new Error('你現在還在活動當中喔'));
+            }
+
+            return models.activity.createAsync({
+                location: data.location,
+                createdBy: data.userId,
+                start: data.start,
+                end: data.end
+            });
+        })
+        .then(function(activity) {
+            return res.api(activity);
+        })
+        .catch(next);
 
 };
