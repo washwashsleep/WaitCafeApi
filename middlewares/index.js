344a4a5cf9142e70f5e3760b42443e282c37c2da
Simon Sun <blackghost0118@gmail.com>
2015-07-26 14:32:49 +0800

    change some code

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,8 +2,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var morgan = require('morgan');
-// var session = require('cookie-session');
-// var session = require('express-session');
 
 var apiBinding = require('./apiBinding');
 
@@ -17,22 +15,6 @@ module.exports = function(app) {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
-
-    // app.use(cookieSession({
-    //   name: 'session',
-    //   keys: ['key1', 'key2']
-    // }));
-
-    // app.use(session({
-    //   secret: 'keyboard cat',
-    //   cookie: {}
-    // }));
-
-    // app.use(session({
-    //   secret: 'keyboard cat',
-    //   key: 'sid',
-    //   cookie: { secure: false }
-    // }));
 
     /*
      * 加入一個新的 res method，叫做 res.api，回傳 json 全部都用 res.api({});
